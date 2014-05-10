f9f9255822559b5efbdfd7971d6bfc1ff0b41f8f
Nozomu Kaneko <nozom.kaneko@gmail.com>
2014-05-11 00:12:45 +0900

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+requires = [
+]
+
+
+setup(name="bookmatch",
+      install_requires=requires,
+      packages=['bookmatch.crawler'],
+      include_package_data=True,
+)
